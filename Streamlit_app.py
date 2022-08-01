23a0fb8f6e84db8d31328d9965a50574b0a13043
mnsramanujam <60528049+mnsramanujam@users.noreply.github.com>
2022-08-01 16:52:26 +0530

    Update Streamlit_app.py
    to add a row

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -50,3 +50,8 @@ streamlit.text ("The fruit load list contains:")
 # streamlit.text("Hello from Snowflake:")
 # streamlit.text(my_data_row)
 streamlit.dataframe(my_data_rows)
+
+
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+streamlit.write('The user entered ', add_my_fruit)
+my_cur.execute("insert into fruit_load_list values("+ add_my_fruit + "))

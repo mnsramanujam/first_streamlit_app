14450704dbc43168fa683400100c57f50caa5adb
mnsramanujam <60528049+mnsramanujam@users.noreply.github.com>
2022-08-01 17:24:08 +0530

    Update Streamlit_app.py
    try catch

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
 import pandas
-import requests
+# import requests
 import snowflake.connector
+import urllib.error import URLError
 
 streamlit.title("My Parents New Healthy Diner")
 
@@ -26,18 +27,23 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 
+
 streamlit.header("Fruityvice Fruit Advice!")
-# fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "watermelon")
-# streamlit.text(fruityvice_response.json())
+try: 
+	fruit_choice = streamlit.text_input('What fruit would you like information about?')
+	if not fruit_choice:
+		streamlit.error("Please select a fruit to get information.")
+	else:
+		fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+		fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+		streamlit.dataframe(fruityvice_normalized)
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+except URLError as e:
+	streamlit.error()
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
+
+
+streamlit.stop()
 
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
@@ -54,4 +60,4 @@ streamlit.dataframe(my_data_rows)
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 streamlit.write('The user entered ', add_my_fruit)
-# my_cur.execute("insert into fruit_load_list values("+ add_my_fruit + "))
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
